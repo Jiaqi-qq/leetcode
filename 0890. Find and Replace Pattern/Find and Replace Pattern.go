c07b9cf72b1a5f98a67af029c56ff71f744c7d7d
Nirvana <1849805767@qq.com>
2022-06-12 17:37:26 +0800

    890. 查找和替换模式 && 题号与题目添加空格

---
+++ 0890. Find and Replace Pattern/Find and Replace Pattern.go
@@ -0,0 +1,24 @@
+// https://leetcode.cn/problems/find-and-replace-pattern/
+package main
+
+func findAndReplacePattern(words []string, pattern string) (ans []string) {
+	for _, word := range words {
+		if match(word, pattern) && match(pattern, word) {
+			ans = append(ans, word)
+		}
+	}
+	return
+}
+
+func match(word, pattern string) bool {
+	mp := map[rune]byte{}
+	for i, x := range word {
+		y := pattern[i]
+		if mp[x] == 0 {
+			mp[x] = y
+		} else if mp[x] != y { // word 中的同一字母必须映射到 pattern 中的同一字母上
+			return false
+		}
+	}
+	return true
+}
